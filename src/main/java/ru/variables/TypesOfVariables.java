85add259ad674047da6d488a6bb73e3f6c8a12ce
vafel745 <vaflvafl47@gmail.com>
2025-07-05 16:20:20 +0300

    1.1 Определите тип переменных и 1.2 Создайте переменные

---
+++ src/main/java/ru/variables/TypesOfVariables.java
@@ -0,0 +1,12 @@
+package ru.variables;
+
+public class TypesOfVariables {
+    byte age = 21;
+    char gender = 'М';
+    float height = 1.75F;
+    int brick = 22032;
+    int bacteria = 10000000;
+    int distance = 405500;
+    boolean workPermit = true;
+    int fileSize = 21343;
+}
