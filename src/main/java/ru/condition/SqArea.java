a89127d976e79873cf27b929284539fc40ffe6e1
vafel745 <vaflvafl47@gmail.com>
2025-07-14 15:53:28 +0300

    2.6 Тесты для стороны прямоугольника

--- src/main/java/ru/condition/SqArea.java
+++ src/main/java/ru/condition/SqArea.java
@@ -1,7 +1,7 @@
 package ru.condition;
 
 public class SqArea {
-    public static double square(double p, double k) {
+    public static double square(int p, double k) {
         double h = p / (2 * (k + 1));
         double l = h * k;
         double s = h * k;
@@ -12,7 +12,5 @@ public class SqArea {
     public static void main(String[] args) {
         double result1 = SqArea.square(4, 1);
         System.out.println(" p = 4, k = 1, s = 1, real = " + result1);
-        double result2 = SqArea.square(6, 2);
-        System.out.println(" p = 6, k = 2, s = 2, real = " + result2);
     }
 }
