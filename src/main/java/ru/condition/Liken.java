cf1075bbef59fab73acfd69e9a89939f24002901
vafel745 <vaflvafl47@gmail.com>
2025-07-14 17:08:24 +0300

    3. Операции сравнения

--- src/main/java/ru/condition/Liken.java
+++ src/main/java/ru/condition/Liken.java
@@ -6,12 +6,12 @@ public class Liken {
         int second = 9;
 
         boolean result = first > second;
-                System.out.println(result);
+        System.out.println(result);
 
         result = first < second;
-                System.out.println(result);
+        System.out.println(result);
 
         result = first == second;
-                System.out.println(result);
+        System.out.println(result);
     }
 }
