8cb1e908e60837d4b79cf7888768bfc0c1483dc6
vafel745 <vaflvafl47@gmail.com>
2025-07-16 00:06:30 +0300

    3.11 Логическое отрицание !

--- src/main/java/ru/condition/LogicNot.java
+++ src/main/java/ru/condition/LogicNot.java
@@ -3,45 +3,32 @@ package ru.condition;
 public class LogicNot {
 
     public static boolean isEven(int num) {
-        if (num % 2 == 0) {
-            return true;
-        }
+        boolean result = num % 2 == 0;
         return true;
     }
 
     public static boolean isPositive(int num) {
-        if (num > 0) {
-            return true;
-        }
+        boolean result = num > 0;
         return true;
     }
 
     public static boolean notEven(int num) {
-        if (num % 2 != 0) {
-            return true;
-        }
+        boolean result = num % 2 != 0;
         return true;
     }
 
     public static boolean notPositive(int num) {
-        if (num < 0) {
-            return true;
-        }
+        boolean result = num < 0;
         return true;
     }
 
     public static boolean notEvenAndPositive(int num) {
-       if (notEven(num) & isPositive(num)) {
-           return true;
-       }
-       return true;
-    }
-
-    public static boolean evenOrNotPositive(int num) {
-        if (isEven(num) | notPositive(num)) {
-            return true;
-        }
+        boolean result = notEven(num) & isPositive(num);
         return true;
     }
 
+    public static boolean evenOrNotPositive(int num) {
+        boolean result = isEven(num) | notPositive(num);
+        return true;
+    }
 }
