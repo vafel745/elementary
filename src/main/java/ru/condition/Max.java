1299efe7787b5a6fdb0c162dfbb676f1a709d34f
vafel745 <vaflvafl47@gmail.com>
2025-07-15 21:46:11 +0300

    3.7 Тернарное сравнение

---
+++ src/main/java/ru/condition/Max.java
@@ -0,0 +1,12 @@
+package ru.condition;
+
+public class Max {
+    public static int max(int left, int right) {
+        int result = left >= right ? left : right;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(max(1, 2));
+    }
+}
