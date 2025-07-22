897fbeea92cb836d4a0e2683be856674ef2cd77d
vafel745 <vaflvafl47@gmail.com>
2025-07-22 15:29:22 +0300

    3.14 Math.abs

--- src/main/java/ru/condition/ChessBoard.java
+++ src/main/java/ru/condition/ChessBoard.java
@@ -4,11 +4,14 @@ public class ChessBoard {
 
     public static int way(int x1, int y1, int x2, int y2) {
         int rsl = 0;
+        int diagonalX = Math.abs(x2 - x1);
+        int diagonalY = Math.abs(y2 - y1);
+
         if (isValid(x1) && isValid(x2)
                 && isValid(y1) && isValid(y2)) {
-            if (x1 != x2 && y1 != y2) {
+            if (diagonalX == diagonalY) {
                 rsl = Math.abs(x2 - x1);
-                rsl = rsl == Math.abs(y2 - y1) ? Math.abs(x2 - x1) : 0;
+                rsl = rsl == 0 ? 0 : Math.abs(x2 - x1);
             }
         }
         return rsl;
