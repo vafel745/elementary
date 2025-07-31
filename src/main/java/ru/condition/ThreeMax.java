be86bd84d556f8e1b46ad52fe3a7649c087ddcfa
vafel745 <vaflvafl47@gmail.com>
2025-07-31 17:31:01 +0300

    3.16 Отладка программы в IDEA

--- src/main/java/ru/condition/ThreeMax.java
+++ src/main/java/ru/condition/ThreeMax.java
@@ -3,10 +3,10 @@ package ru.condition;
 public class ThreeMax {
     public static int max(int first, int second, int third) {
         int result = first;
-        if (first < second || first < third) {
+        if (second > first && second > third) {
             result = second;
         }
-        if (third > second && third > first) {
+        if (third > second || third > first) {
             result = third;
         }
         return result;
