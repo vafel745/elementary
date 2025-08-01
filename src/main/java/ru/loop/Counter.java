b1fb07730034d83c65f5e1bb6de0aaa7ed4e91a8
vafel745 <vaflvafl47@gmail.com>
2025-08-01 14:39:53 +0300

    4.2 Сумма чисел

---
+++ src/main/java/ru/loop/Counter.java
@@ -0,0 +1,18 @@
+package ru.loop;
+
+public class Counter {
+    public static int sum(int start, int finish) {
+        int result = 0;
+        for (int index = 0; index <= finish; index++) {
+            start += index;
+            result = start;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(0, 10));
+        System.out.println(sum(3, 8));
+        System.out.println(sum(1, 1));
+    }
+}
