2fc74da12c06ae8a3586351c3498a2170e8cb546
vafel745 <vaflvafl47@gmail.com>
2025-08-27 23:50:41 +0300

    4.8 Простое число оператор прерывания break

--- src/main/java/ru/loop/CheckPrimeNumber.java
+++ src/main/java/ru/loop/CheckPrimeNumber.java
@@ -2,15 +2,9 @@ package ru.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean prime = true;
-        for (int index = 1; index <= number; index++) {
-            if (number == 1) {
-                prime = false;
-                break;
-            } else if (number % index == 0 && number % 2 != 0) {
-                prime = true;
-                break;
-            } else {
+        boolean prime = number > 1;
+        for (int index = 2; index < number; index++) {
+            if (number % index == 0) {
                 prime = false;
                 break;
             }
--- src/test/java/ru/loop/CheckPrimeNumberTest.java
+++ src/test/java/ru/loop/CheckPrimeNumberTest.java
@@ -5,8 +5,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class CheckPrimeNumberTest {
     @Test
-    void when5() {
-        int number = 5;
+    void when1() {
+        int number = 1;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void when2() {
+        int number = 2;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void when3() {
+        int number = 3;
         boolean result = CheckPrimeNumber.check(number);
         assertThat(result).isTrue();
     }
@@ -19,15 +33,183 @@ class CheckPrimeNumberTest {
     }
 
     @Test
-    void when2() {
-        int number = 2;
+    void when5() {
+        int number = 5;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void when6() {
+        int number = 6;
         boolean result = CheckPrimeNumber.check(number);
         assertThat(result).isFalse();
     }
 
     @Test
-    void when1() {
-        int number = 1;
+    void when7() {
+        int number = 7;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void when8() {
+        int number = 8;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void when9() {
+        int number = 9;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void when10() {
+        int number = 10;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void when11() {
+        int number = 11;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void when12() {
+        int number = 12;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void when13() {
+        int number = 13;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void when14() {
+        int number = 14;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void when15() {
+        int number = 15;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void when16() {
+        int number = 16;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void when17() {
+        int number = 17;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void when18() {
+        int number = 18;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void when19() {
+        int number = 19;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void when20() {
+        int number = 20;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void when21() {
+        int number = 21;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void when22() {
+        int number = 22;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void when23() {
+        int number = 23;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void when24() {
+        int number = 24;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void when25() {
+        int number = 25;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void when26() {
+        int number = 26;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void when27() {
+        int number = 27;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void when28() {
+        int number = 28;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void when29() {
+        int number = 29;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void when30() {
+        int number = 30;
         boolean result = CheckPrimeNumber.check(number);
         assertThat(result).isFalse();
     }
