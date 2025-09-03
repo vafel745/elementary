6e4dc02d112528a55b4f6b581645a274a087b741
vafel745 <vaflvafl47@gmail.com>
2025-09-03 21:29:24 +0300

    5.14 Слово начинается с...

---
+++ src/main/java/ru/array/ArrayChar.java
@@ -0,0 +1,14 @@
+package ru.array;
+
+public class ArrayChar {
+
+    public static boolean startsWith(char[] word, char[] pref) {
+        boolean result = true;
+        for (int i = 0; i < pref.length; i++) {
+            if (word[i] != pref[i]) {
+                return false;
+            }
+        }
+        return result;
+    }
+}
