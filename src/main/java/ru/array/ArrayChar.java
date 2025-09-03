9747cb5c829daa084c3b33d75843cde3fd2a61df
vafel745 <vaflvafl47@gmail.com>
2025-09-03 22:49:31 +0300

    5.14 Слово начинается с...

--- src/main/java/ru/array/ArrayChar.java
+++ src/main/java/ru/array/ArrayChar.java
@@ -6,7 +6,8 @@ public class ArrayChar {
         boolean result = true;
         for (int i = 0; i < pref.length; i++) {
             if (word[i] != pref[i]) {
-                return false;
+                result = false;
+                break;
             }
         }
         return result;
