a1958c18bf8ac8df113036ad67f5d925fd1c9df9
vafel745 <vaflvafl47@gmail.com>
2025-09-04 17:58:34 +0300

    5.18 Поиск индекса в диапазоне

--- src/main/java/ru/array/FindLoop.java
+++ src/main/java/ru/array/FindLoop.java
@@ -10,4 +10,15 @@ public class FindLoop {
         }
         return rst;
     }
+
+    public static int indexInRange(int[] data, int el, int start, int finish) {
+        int rst = -1;
+        for (int i = start; i <= finish; i++) {
+            if (data[i] == el) {
+                return i;
+            }
+        }
+        return rst;
+    }
+
 }
