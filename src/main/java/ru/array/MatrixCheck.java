a01656bb1e2550dc6af0990e7b0bf9cb3679ba9e
vafel745 <vaflvafl47@gmail.com>
2025-09-08 23:45:54 +0300

    5.27 Моно строка в матрице

--- src/main/java/ru/array/MatrixCheck.java
+++ src/main/java/ru/array/MatrixCheck.java
@@ -4,13 +4,10 @@ public class MatrixCheck {
     public static boolean monoHorizontal(char[][] board, int row) {
         boolean res = true;
         for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                if (board[row][j] != 'X') {
+                if (board[row][i] != 'X') {
                     res = false;
                     break;
                 }
-
-            }
         }
         return res;
     }
