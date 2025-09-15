e5662dab4011522cbdf814a0b78b31dc15e1d891
vafel745 <vaflvafl47@gmail.com>
2025-09-16 00:36:56 +0300

    5.31 Метод двух указателей

--- src/test/java/ru/array/TwoNumberSum.java
+++ src/test/java/ru/array/TwoNumberSum.java
@@ -5,7 +5,7 @@ public class TwoNumberSum {
         int i = 0;
         int j = array.length - 1;
         while (array[i] + array[j] != target) {
-            if (i == array.length - 1 || j == 0) {
+            if ((i == array.length / 2)  || (j == array.length / 2)) {
                 return new int[]{};
             } else if (array[i] + array[j] < target) {
                 i++;
