0e861a566a1fd45a72392985bb9c5a6ab673da09
vafel745 <vaflvafl47@gmail.com>
2025-10-19 21:34:53 +0300

    5.32 Жадный алгоритм. Сдача в кофе машине (while)

--- src/main/java/ru/array/Machine.java
+++ src/main/java/ru/array/Machine.java
@@ -8,7 +8,7 @@ public class Machine {
         int[] rsl = new int[100];
         int size = 0;
         int change = money - price;
-        for (int coin:coins) {
+        for (int coin : coins) {
             while (change >= coin) {
                 change -= coin;
                 rsl[size] = coin;
